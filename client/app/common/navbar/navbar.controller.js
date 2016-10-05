249dcaee38813ce500ee2f33ffb6837f17140eaa
juandjara <juanorigami@gmail.com>
2016-10-05 15:21:37 +0200

    feat:  boilerplate changes

--- client/app/common/navbar/navbar.controller.js
+++ client/app/common/navbar/navbar.controller.js
@@ -1,7 +1,19 @@
 class NavbarController {
-  constructor() {
+  constructor($log) {
+    "ngInject";
+    inject(this, arguments, NavbarController.$inject);
     this.name = 'navbar';
   }
+  test(){
+    this.$log.debug("Hello from the navbar");
+  }
+}
+
+function inject(instance, dependencies, dependency_names){
+  [].slice.call(dependencies, 0).forEach(function(dep, index){
+    let depname = dependency_names[index]
+    instance[depname] = dep;
+  });
 }
 
 export default NavbarController;
