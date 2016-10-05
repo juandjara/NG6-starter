b175d5768dadb3549f4161e5d9a8d07d8483a279
juandjara <juanorigami@gmail.com>
2016-10-05 15:30:09 +0200

    feat: template

--- client/app/common/navbar/navbar.controller.js
+++ client/app/common/navbar/navbar.controller.js
@@ -1,16 +1,16 @@
 class NavbarController {
   constructor($log) {
     "ngInject";
-    inject(this, arguments, NavbarController.$inject);
+    postInject(this, arguments, NavbarController.$inject);
     this.name = 'navbar';
   }
-  test(){
-    this.$log.debug("Hello from the navbar");
+  sayHi(){
+    this.$log.debug("Hello from the navbar!");
   }
 }
 
-function inject(instance, dependencies, dependency_names){
-  [].slice.call(dependencies, 0).forEach(function(dep, index){
+function postInject(instance, dependencies, dependency_names){
+  [].forEach.call(dependencies, function(dep, index){
     let depname = dependency_names[index]
     instance[depname] = dep;
   });
